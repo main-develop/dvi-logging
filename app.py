ec913374c4a9dcd587ab60cbd6419eed6b9ab16a
dev <main.develop@protonmail.com>
2025-01-28 14:46:00 +0100

    Added settings route (prev. personal information route)

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_cors import CORS
 from routes.navigation_route import navigation
 from routes.authentication_route import authentication
+from routes.settings import settings
 
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(navigation, url_prefix="/dvi-logging/navigation")
     app.register_blueprint(authentication, url_prefix="/dvi-logging/authentication")
+    app.register_blueprint(settings, url_prefix="/dvi-logging/settings")
 
     CORS(app)
 
